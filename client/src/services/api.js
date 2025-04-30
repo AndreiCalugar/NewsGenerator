3134cb311afc6f85654f9008ea8a5514defb08b8
Andrei <andrei.nicolae.calugar@gmail.com>
2025-04-30 13:47:24 +0300

    Prepare backend for deployment

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,8 @@
 // client/src/services/api.js
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL =
+  process.env.REACT_APP_API_URL || "https://your-render-app-name.onrender.com";
 
 // Create axios instance with default config
 const axiosInstance = axios.create({
